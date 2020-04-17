ca7272ddeefa1703e31612532e180c8d25a519eb
Charis <programatisths@gmail.com>
2020-04-18 02:13:16 +0300

    Added template to run project as api

---
+++ src/main/java/com/cmoutafidis/lambdaexample/Handler.java
@@ -0,0 +1,39 @@
+package com.cmoutafidis.lambdaexample;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Handler implements RequestHandler<Object, GatewayResponse> {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Override
+    public GatewayResponse handleRequest(final Object event, final Context context) {
+        final Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+        try {
+            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+            final String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+            return new GatewayResponse(output, headers, 200);
+        } catch (final IOException e) {
+            return new GatewayResponse("{}", headers, 500);
+        }
+    }
+
+    private String getPageContents(final String address) throws IOException {
+        final URL url = new URL(address);
+        try (final BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+}
